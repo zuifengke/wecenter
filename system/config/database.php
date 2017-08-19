0673f2ba10f8654c8ff20981d4cedb0a53d70df1
zuifengke <zuifengke@DESKTOP-L0DIMB0>
2017-08-19 11:30:21 +0800

    天天

---
+++ system/config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => 'root',
+  'dbname' => 'tech-heren-com',
+  'port' => '3306',
+);
+$config['slave'] = false;
+$config['port'] = '3306';
